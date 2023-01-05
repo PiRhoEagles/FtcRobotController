96780477f8778f408b2a2f1eddc49baba6222323
Owen Slagle <owenslagle@blueyoshi.com>
2023-01-05 17:02:51 -0500

    Finished Basic Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoTesting.java
@@ -14,12 +14,15 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 public class AutoTesting extends BaseAuto{
 
     public static int autoToRun = 2;
-    public static int x1 = 51;
-    public static int y1 = 4;
-    public static double h1 = Math.toRadians(45);
-    public static int x2 = 51;
-    public static int y2 = -10;
-    public static double h2 = Math.toRadians(-90);
+
+    public static int x1 = 50;
+    public static int y1 = 0;
+    public static int h1 = -82;
+
+    public static int x2 = 50;
+    public static int y2 = 3;
+    public static int h2 = 45;
+
 
     @Override
     public TrajectorySequence trajectorySequenceBuilder(PowerPlayPipeline.detectionStates detectionState) {
@@ -42,18 +45,44 @@ public class AutoTesting extends BaseAuto{
                         .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.STACK2)) // raise cone a little
                         .lineToSplineHeading(new Pose2d(43,0,Math.toRadians(0))) // move near pole
                         .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.HIGH)) // start raising slides to high
-                        .lineToSplineHeading(new Pose2d(56,9,Math.toRadians(45))) // turn to face pole and place cone over it
+
+                        .lineToSplineHeading(new Pose2d(55,5,Math.toRadians(45))) // turn to face pole and place cone over it
                         .waitSeconds(0.5) // wait for slides to raise
                         .addTemporalMarker(() -> mechanism.openClaw()) // drops cone on pole
                         .waitSeconds(0.5) // wait for grabber to open
                         .lineToSplineHeading(new Pose2d(51,4,Math.toRadians(45))) // move back from pole
                         .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.STACK5)) // lower slides
                         .lineToSplineHeading(new Pose2d(50,-5,Math.toRadians(-82))) // move towards and face cone stack
-                        .lineToSplineHeading(new Pose2d(54,-25,Math.toRadians(-82))) // move to cone stack
-                        .addTemporalMarker(() -> mechanism.closeClaw()) // close claw
+                        .lineToSplineHeading(new Pose2d(53,-26,Math.toRadians(-82))) // move to cone stack
+                        .addTemporalMarker(() -> mechanism.closeClaw()) // close claw and grab cone
                         .waitSeconds(0.5) // wait for claw to close
-                        .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.LOW)) // raise the slides above the cone stack
+                        .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.HIGH)) // raise the slides above the cone stack
                         .lineToSplineHeading(new Pose2d(50,0,Math.toRadians(-82))) // move away from cone stack
+                        .lineToSplineHeading(new Pose2d(50,3,Math.toRadians(45))) // move to face pole
+
+                        .lineToSplineHeading(new Pose2d(55,5,Math.toRadians(45))) // turn to pole and place cone over it
+                        .waitSeconds(0.5) // wait for slides to raise
+                        .addTemporalMarker(() -> mechanism.openClaw()) // drops cone on pole
+                        .waitSeconds(0.5) // wait for grabber to open
+                        .lineToSplineHeading(new Pose2d(51,4,Math.toRadians(45))) // move back from pole
+                        .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.STACK4)) // lower slides
+                        .lineToSplineHeading(new Pose2d(50,-5,Math.toRadians(-82))) // move towards and face cone stack
+                        .lineToSplineHeading(new Pose2d(53,-26,Math.toRadians(-82))) // move to cone stack
+                        .addTemporalMarker(() -> mechanism.closeClaw()) // close claw and grab cone
+                        .waitSeconds(0.5) // wait for claw to close
+                        .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.HIGH)) // raise the slides above the cone stack
+                        .lineToSplineHeading(new Pose2d(50,0,Math.toRadians(-82))) // move away from cone stack
+                        .lineToSplineHeading(new Pose2d(50,3,Math.toRadians(45))) // move to face pole
+
+                        .lineToSplineHeading(new Pose2d(55,5,Math.toRadians(45))) // turn to pole and place cone over it
+                        .waitSeconds(0.5) // wait for slides to raise
+                        .addTemporalMarker(() -> mechanism.openClaw()) // drops cone on pole
+                        .waitSeconds(0.5) // wait for grabber to open
+                        .lineToSplineHeading(new Pose2d(51,1,Math.toRadians(45))) // move back from pole
+                        .addTemporalMarker(() -> mechanism.moveSlides(ScoringMechanism.slidePositions.FLOOR)) // lower slides
+
+                        .lineToSplineHeading(new Pose2d(50,0,Math.toRadians(82))) // move to position 2
+
                         .build();
             case 3: // turn test
                 return drive.trajectorySequenceBuilder(new Pose2d(0,0,Math.toRadians(0)))
